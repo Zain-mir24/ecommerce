c7445cb783d94e740cc004296bb5c746277af560
Zainmir <zainnaeemk10@gmail.com>
2021-01-27 16:24:01 +0000

    adding validation to productSchema

---
+++ modules/adminmodule/validator.js
@@ -0,0 +1,12 @@
+const { body, validationResult } = require('express-validator');
+const validate=(req,res)=>{
+    body('Description').isString().withMessage('must be a string')
+    body('Price').isInt().withMessage('must be a number'),
+    body('image').isString().withMessage('must provide image path in string '),
+    body('color').isString().withMessage('Color must be a string '),
+    body('Ram').isInt().withMessage('must be a number'),
+    body('Rom').isInt().withMessage('must be a number'),
+
+    
+}
+module.exports.validate =validate
