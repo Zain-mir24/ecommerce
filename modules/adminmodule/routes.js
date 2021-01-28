97e367b56621ef196a8a09eec172d2660dfa7f16
Zainmir <zainnaeemk10@gmail.com>
2021-01-28 13:51:18 +0000

    validator

--- modules/adminmodule/routes.js
+++ modules/adminmodule/routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const product = require('../../schema/productSchema')
 const controller = require('./controller')
+    
 const validator = require('./validator')
 router.get("/", function (req, res) {
     res.send("<h1>Hello World</h1>")
 });
-router.post("/addproduct",validator.validate(req.body),async(req,res)=>{
+router.post("/addproduct",validator.validate,async(req,res)=>{
    controller.addproduct(req, res)
 })
 router.post("/searchproduct",(req,res)=>{
--- modules/adminmodule/validator.js
+++ modules/adminmodule/validator.js
@@ -5,7 +5,7 @@ const validate=(req,res)=>{
     body('image').isString().withMessage('must provide image path in string '),
     body('color').isString().withMessage('Color must be a string '),
     body('Ram').isInt().withMessage('must be a number'),
-    body('Rom').isInt().withMessage('must be a number'),
+    body('Rom').isInt().withMessage('must be a number')
 
     
 }
