d5c00efbb1ba9d7ec673c74493c2aded60f340a7
Zainmir <zainnaeemk10@gmail.com>
2021-01-28 16:39:05 +0000

    valid

--- modules/adminmodule/routes.js
+++ modules/adminmodule/routes.js
@@ -2,13 +2,26 @@ const express = require('express');
 const router = express.Router();
 const product = require('../../schema/productSchema')
 const controller = require('./controller')
-    
+const {validationResult,body} =require('express-validator')
 const validator = require('./validator')
 router.get("/", function (req, res) {
     res.send("<h1>Hello World</h1>")
 });
-router.post("/addproduct",validator.validate,async(req,res)=>{
-   controller.addproduct(req, res)
+router.post("/addproduct",validator.validate("createProduct"),async(req,res)=>{
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        controller.addproduct(req, res,next)
+        // Return a response to client.
+    } catch (err) {
+        
+        
+    }
+   
 })
 router.post("/searchproduct",(req,res)=>{
     controller.seachproduct(req, res)
--- modules/adminmodule/validator.js
+++ modules/adminmodule/validator.js
@@ -1,12 +1,33 @@
-const { body, validationResult } = require('express-validator');
-const validate=(req,res)=>{
-    body('Description').isString().withMessage('must be a string')
-    body('Price').isInt().withMessage('must be a number'),
-    body('image').isString().withMessage('must provide image path in string '),
-    body('color').isString().withMessage('Color must be a string '),
-    body('Ram').isInt().withMessage('must be a number'),
-    body('Rom').isInt().withMessage('must be a number')
+const { body, validationResult } = require('express-validator')
+const Product = require('../../schema/productSchema')
 
-    
+exports.validate = (method) => {
+    switch (method) {
+        case 'createProduct': {
+
+            return [
+            body('Description','The value must be a String').isString(),
+            body('Price','the value must be an integer').isInt(),
+         
+            ]
+        }
+        case 'searchProduct': {
+            return [
+                body('Pid').exists().isMongoId()
+
+            ]
+        }
+        case 'deleteproduct': {
+            return [
+                body('Pid').exists().isMongoId()
+            ]
+        }
+        case "addToWishlist": {
+            return [
+                body('productID').exists().isMongoId(),
+                body('clientID').exists().isMongoId()
+            ]
+        }
+
+    }
 }
-module.exports.validate =validate
